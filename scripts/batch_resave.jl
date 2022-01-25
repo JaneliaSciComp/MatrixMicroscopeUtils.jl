41e1e9dcf7eb10388f456c6811a28c04d010eaa2
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-01-25 02:52:33 -0500

    Use Distributed in batch_resave.jl script

--- scripts/batch_resave.jl
+++ scripts/batch_resave.jl
@@ -1,7 +1,11 @@
 # Batch resave stack files to HDF5 files
 # Two or three command line args are read: [-n | --mock] in_path out_path
-using Pkg
-Pkg.activate(dirname(@__DIR__))
+using Distributed
 
-using MatrixMicroscopeUtils
+@everywhere begin
+    using Pkg
+    Pkg.activate(dirname(@__DIR__))
+    using MatrixMicroscopeUtils
+end
+
 batch_resave_stacks_as_hdf5()
